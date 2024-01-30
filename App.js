52f4b2de56551f6da1769abc559a8d82ae3eeb6f
neon2k21 <roman_2001@icloud.com>
2024-01-30 13:28:57 +0500

    final-edit-object

--- App.js
+++ App.js
@@ -1,9 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
+import ObjectCard from './components/card_of_object';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Шапка профиля */}
+      <View className="bg-red-500 w-full rounded-b-lg" style={{height:widthPercentageToDP(20)}}>
+        <TouchableOpacity>
+            <Text style={{marginHorizontal:widthPercentageToDP(10),paddingTop:widthPercentageToDP(3)}} className="text-2xl">
+              ФИО
+            </Text>
+            <Text style={{marginHorizontal:widthPercentageToDP(10)}} className="text-base">
+            Перейти в профиль 
+            </Text>
+        </TouchableOpacity>
+        
+      </View>
+            {/* карусель объектов */}
+
+      <View style={{paddingTop:widthPercentageToDP(50)}}>
+        <View className="flex-row bg-red-500" style={{width: widthPercentageToDP(95), height:widthPercentageToDP(10), margin:widthPercentageToDP(2),alignItems:'center'}}>
+        <Text className="text-2xl" style={{}}>
+          Объекты
+        </Text>
+        <TouchableOpacity>
+          <Text className="text-xl" style={{right:-widthPercentageToDP(60)}}>
+          Ещё
+        </Text>
+        </TouchableOpacity>
+        
+        </View>
+        
+          <ObjectCard/>
+          <FlatList>
+
+          </FlatList>
+
+      </View>
+            {/*карусель заявок */}
+            <FlatList>
+
+            </FlatList>
+      <View>
+        
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -12,9 +54,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop:widthPercentageToDP(10),
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
---
+++ components/card_of_object.js
@@ -0,0 +1,37 @@
+import { View, Text, TouchableOpacity, Image } from "react-native"
+import {ArrowRightCircleIcon} from "react-native-heroicons/solid"
+import { widthPercentageToDP } from "react-native-responsive-screen"
+
+//arrow-right-circle
+
+
+export default function ObjectCard(){
+
+
+    return(
+        <TouchableOpacity>
+ <View style={{width:widthPercentageToDP(60),height:widthPercentageToDP(40)}} className="rounded-2xl">
+            <Image  source={require('../assets/DSCF4575.jpg')} 
+                className=" rounded-2xl w-full h-full "/>
+            <View className="rounded-b-2xl w-full absolute flex-row" style={{opacity:0.7,backgroundColor:'gray' ,bottom:-widthPercentageToDP(0.2),height:widthPercentageToDP(15)}}>
+                <View  style={{width:widthPercentageToDP(45)}}>
+                     <Text style={{left:widthPercentageToDP(5),color:'white'}} className="text-xl">
+                        text
+                     </Text>
+                </View>
+            
+            {/* <ArrowRightCircleIcon size={widthPercentageToDP(15)} color={'gray'} /> */}
+            </View>
+            
+
+
+
+        </View>
+        </TouchableOpacity>
+       
+
+
+    )
+
+
+}
